494f81c90321ecc56f6f8e434c7a6b9d2444f654
Maxwell Baird <bairdm71@gmail.com>
2020-04-03 11:33:52 -0600

    retyped the seed for better reading

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,29 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 park = Park.create(name: "Fun Land",
                    admissions: 20.00)
+
 mechanic1 = Mechanic.create(name: "Bob McBob",
                            experience: 5)
 
+mechanic2 = Mechanic.create(name: "Jane Doe",
+                            experience: 13)
+                            
 park.rides.create(name: "Slide of DOOOM",
                   rating: 2,
                   mechanic_id: mechanic1.id)
+
 park.rides.create(name: "Maze of Corn",
                   rating: 10,
                   mechanic_id: mechanic1.id)
+
 park.rides.create(name: "The Upside Down",
                   rating: 6,
                   mechanic_id: mechanic1.id)
-mechanic2 = Mechanic.create(name: "Jane Doe",
-                           experience: 13)
+
+park.rides.create(name: "Cheesy House",
+                  rating: 2,
+                  mechanic_id: mechanic1.id)
+
+park.rides.create(name: "A Small World",
+                  rating: 2,
+                  mechanic_id: mechanic1.id)
