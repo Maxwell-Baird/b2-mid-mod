2954d5ef75d1a6aec7da310263b7523f19b48362
Maxwell Baird <bairdm71@gmail.com>
2020-04-03 09:10:03 -0600

    created the test to check mechanics index page

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor", type: :feature do
+  it "I can see all mechanics at '/mechanics'" do
+
+    mechanic1 = Mechanics.create(name: "Bob McBob",
+                                  experience: 5)
+    mechanic2 = Mechanics.create(name: "Jane Doe",
+                                  experience: 13)
+    visit "/mechanics"
+
+    within("mechanics-#{mechanic1.id}") do
+      expect(page).to have_content(mechanic1.name)
+      expect(page).to have_content(mechanic1.experience)
+    end
+    within("mechanics-#{mechanic2.id}") do
+      expect(page).to have_content(mechanic2.name)
+      expect(page).to have_content(mechanic2.experience)
+    end
+  end
+end
