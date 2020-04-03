3818821a5c4119fb4313cd16d58bca4c74e4c81e
Maxwell Baird <bairdm71@gmail.com>
2020-04-03 11:22:40 -0600

    added seeds to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+park = Park.create(name: "Fun Land",
+                   admissions: 20.00)
+mechanic1 = Mechanic.create(name: "Bob McBob",
+                           experience: 5)
+
+park.rides.create(name: "Slide of DOOOM",
+                  rating: 2,
+                  mechanic_id: mechanic1.id)
+park.rides.create(name: "Maze of Corn",
+                  rating: 10,
+                  mechanic_id: mechanic1.id)
+park.rides.create(name: "The Upside Down",
+                  rating: 6,
+                  mechanic_id: mechanic1.id)
+mechanic2 = Mechanic.create(name: "Jane Doe",
+                           experience: 5)
